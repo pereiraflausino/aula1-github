8df19900f27271892372d507f5f4e035d1c553a2
pereiraflausino <pereiraflausino@gmail.com>
2020-04-19 23:33:12 -0300

    Primeira atualizacao

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+import util.cauculator;
+
+public class Program {	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+				
+		System.out.println("Informe o raio: ");
+		double radius = sc.nextDouble();
+		
+		double c = cauculator.circumference(radius);		
+		double v = cauculator.volume(radius);
+		
+		System.out.printf("Circunference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", cauculator.PI);	
+		
+		
+		sc.close();
+	}
+	
+	
+}
