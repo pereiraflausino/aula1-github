c3aaf5221e952945ffeac1b6ed77cc6692e3a05d
pereiraflausino <pereiraflausino@gmail.com>
2020-04-20 00:05:24 -0300

    alteração 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,17 +12,6 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 				
-		System.out.println("Informe o raio: ");
-		double radius = sc.nextDouble();
-		
-		double c = cauculator.circumference(radius);		
-		double v = cauculator.volume(radius);
-		
-		System.out.printf("Circunference: %.2f%n", c);
-		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", cauculator.PI);	
-		
-		
 		sc.close();
 	}
 	
